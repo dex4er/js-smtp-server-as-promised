18f94377c6ef4c5040b015887229881aba02f4a9
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-10-26 12:40:39 +0200

    Formatting

--- examples/test-server-with-callbacks.js
+++ examples/test-server-with-callbacks.js
@@ -6,7 +6,7 @@ const SMTPServer = require('smtp-server').SMTPServer
 
 // Usage: node server.js opt1=value1 opt2=value2...
 const options = Object.assign({})//, ...process.argv.slice(2).map(a => a.split('=')).map(([k, v]) => ({[k]: v})))
-Object.assign(options, {disabledCommands: ['AUTH'], onConnect, onMailFrom, onRcptTo, onData, onClose})
+Object.assign(options, { disabledCommands: ['AUTH'], onConnect, onMailFrom, onRcptTo, onData, onClose })
 
 function onConnect (session, callback) {
   console.log(`[${session.id}] onConnect`)
@@ -17,7 +17,7 @@ function onMailFrom (from, session, callback) {
   console.log(`[${session.id}] onMailFrom ${from.address}`)
   if (from.address.split('@')[1] === 'spammer.com') {
     // code 421 disconnects SMTP session immediately
-    return callback(Object.assign(new Error('we do not like spam!'), {responseCode: 421}))
+    return callback(Object.assign(new Error('we do not like spam!'), { responseCode: 421 }))
   }
   callback()
 }

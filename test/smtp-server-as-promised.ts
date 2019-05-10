1c0968443f049fcf350f2ed0aaafb544bf1246b0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-05-10 15:21:31 +0200

    Use Chai expect API

--- test/smtp-server-as-promised.ts
+++ test/smtp-server-as-promised.ts
@@ -1,7 +1,6 @@
 import {After, And, Feature, Given, Scenario, Then, When} from './lib/steps'
 
-import chai from 'chai'
-chai.should()
+import chai, {expect} from 'chai'
 
 import chaiAsPromised from 'chai-as-promised'
 chai.use(chaiAsPromised)
@@ -33,19 +32,14 @@ Feature('Test smtp-server-as-promised module', () => {
     }
   }
 
+  // prettier-ignore
   const rfc2822Message =
     '' +
-    'From: sender@example.com' +
-    crlf +
-    'To: recipient@example.net' +
-    crlf +
-    'Subject: test' +
-    crlf +
-    crlf +
-    'Test' +
-    crlf +
-    '.' +
-    crlf
+    'From: sender@example.com' + crlf +
+    'To: recipient@example.net' + crlf +
+    'Subject: test' + crlf + crlf +
+    'Test' + crlf +
+    '.' + crlf
 
   const authPlainString = Buffer.from('\0username\0password').toString('base64')
 
@@ -69,8 +63,10 @@ Feature('Test smtp-server-as-promised module', () => {
       address = await server.listen({port: 0})
     })
 
-    Then('port number should be correct', () => {
-      address.port.should.be.above(1024).and.below(65535)
+    Then('port number) to be correct', () => {
+      expect(address.port)
+        .to.be.above(1024)
+        .and.below(65535)
     })
 
     When('I connect to the server', async () => {
@@ -79,7 +75,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get SMTP banner', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^220 .* ESMTP\r\n$/)
+      expect(chunk!.toString()).to.match(/^220 .* ESMTP\r\n$/)
     })
 
     When('I send EHLO command', async () => {
@@ -88,7 +84,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get EHLO response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^250-/)
+      expect(chunk!.toString()).to.match(/^250-/)
     })
 
     When('I send AUTH PLAIN command', async () => {
@@ -97,7 +93,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get AUTH PLAIN response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^235 Authentication successful/)
+      expect(chunk!.toString()).to.match(/^235 Authentication successful/)
     })
 
     When('I send MAIL FROM command', async () => {
@@ -106,7 +102,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get MAIL FROM response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^250 Accepted/)
+      expect(chunk!.toString()).to.match(/^250 Accepted/)
     })
 
     When('I send RCPT TO command', async () => {
@@ -115,7 +111,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get RCPT TO response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^250 Accepted/)
+      expect(chunk!.toString()).to.match(/^250 Accepted/)
     })
 
     When('I send DATA command', async () => {
@@ -124,7 +120,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get DATA response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^354 End data/)
+      expect(chunk!.toString()).to.match(/^354 End data/)
     })
 
     When('I send an RFC2822 message', async () => {
@@ -137,7 +133,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
     Then('I get message queued response', async () => {
       const chunk = await client.read()
-      chunk!.toString().should.match(/^250 OK: message queued/)
+      expect(chunk!.toString()).to.match(/^250 OK: message queued/)
     })
 
     if (semver.gte(process.version, 'v10.0.0')) {
@@ -147,7 +143,7 @@ Feature('Test smtp-server-as-promised module', () => {
 
       Then('I get QUIT response', async () => {
         const chunk = await client.read()
-        chunk!.toString().should.match(/^221 Bye/)
+        expect(chunk!.toString()).to.match(/^221 Bye/)
       })
     } else {
       // Not correct synchronization on Node < 10 because it misses
@@ -160,13 +156,13 @@ Feature('Test smtp-server-as-promised module', () => {
       })
 
       Then('I get QUIT response', () => {
-        lastChunk!.toString().should.match(/^221 Bye/)
+        expect(lastChunk!.toString()).to.match(/^221 Bye/)
       })
     }
 
     And('there is nothing more to read', async () => {
       const chunk = await client.read()
-      return (chunk === undefined).should.be.true
+      return expect(chunk).to.be.undefined
     })
 
     After(async () => {
